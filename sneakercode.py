f0371cf5c4c2e4e5459d5b193ca344fbd9879dac
Quatez12 <quatez3409@icloud.com>
2025-04-18 07:16:14 -0400

    Update sneakercode.py

--- sneakercode.py
+++ sneakercode.py
@@ -1,7 +1,17 @@
+#Sneaker flashcard welcome page
 import tkinter as tk
 
 def welcome_page():
-  root = tk.tk()
-  
+  root = tk.Tk() 
+  root.title(" Sneaker Flash Card Game")
 
+label = tk.Label(root, text="Ready to Play?!", font=("Helvetica, 24))
+label.pack(pady=20)
 
+start_button = tk.Button(root, text-"Start Game", command = root.quit font="Helvetica", 16))
+start_button.pack(pady=10
+
+root.mainloop()
+
+if __name == "__main__" 
+  welcome_page()
