0d6bc81f13726b910fc02ba2d561601af796aa31
Quatez12 <quatez3409@icloud.com>
2025-04-18 07:34:54 -0400

    Update sneakercode.py

--- sneakercode.py
+++ sneakercode.py
@@ -1,5 +1,15 @@
 #Sneaker flashcard welcome page
 import tkinter as tk
+from tkinter import messagebox
+from PIL import image, ImageTK
+import requests
+from io import BytesIO
+
+def show_image():
+  response = requests.get("https://github.com/ksu-is/Quatez-Sneaker-Repository-Project-/blob/main/Image%204-5-25%20at%2010.36%20AM.JPG")
+  img_data = responses.content
+  img = Image.open(BytesIO(img_data))
+  img.show()
 
 def welcome_page():
   root = tk.Tk() 
